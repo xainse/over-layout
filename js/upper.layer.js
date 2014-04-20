c612b963dd6640aee6ba71fe3274e2906f86e296
Sergey Kholin <xainSE@gmail.com>
2014-04-20 14:09:03 +0300

    change settings calculation

--- js/upper.layer.js
+++ js/upper.layer.js
@@ -1,7 +1,10 @@
 /**
  * @author @xainse
+ * Скрипт написан для того, что-бы подгонять верстку страницы под макет полученный от дизайнера.
+ * В исходнике страницы, над которой рабтаем, нужно загрузить скрипт и передать настройки с путем к макету.
+ * TODO: 1. нужно добавить рабочий пример.
+ * TODO: 2. нужно вывести на странице подсказку, что стиль загруен
  */
-
 
 var upperLayout = {
 	
@@ -10,38 +13,58 @@ var upperLayout = {
 	layer_lft_pos: 0,
 	
 	//layer_wrap: '<div class="layer" id="upper-layer" style="background:url(img/maket.jpg) no-repeat 50% 0 transparent; display:none; position: absolute; top:0; left:0; width:100%; height: 800px; opacity:0.5;"></div>',
-	
 	layer_id: 'upper-layer',
-		
+
+    /**
+     * Вызов скрипта настроек и передача параметров
+     * @param settings
+     */
 	setup: function (settings) {
-		
 		this.initSettings(settings);				
 		this.addMaketLayer();		
 	},
-	
+
+    /**
+     * Create transparent maket over the html page, to correct
+     */
 	addMaketLayer: function () {
 		var newDiv = document.createElement('div');
 		newDiv.className = 'layer';
-		newDiv.id = 'upper-layer';
+		newDiv.id = this.layer_id;
 			newDiv.style.background	= "url("+this.layer_img+") no-repeat 50% 0 transparent";
-			newDiv.style.display = 'none'; 
+			newDiv.style.display    = 'none';
 			newDiv.style.position	= 'absolute'; 
-			newDiv.style.top		= 0; 
-			newDiv.style.left		= 0; 
-			newDiv.style.width		= '100%'; 
-			newDiv.style.height		= '800px'; 
-			newDiv.style.opacity	= '0.5'; //.backgroundColor = 'red';
-			newDiv.style.zIndex		= 1000;
+			newDiv.style.top		= this.top; 
+			newDiv.style.left		= this.left;
+			newDiv.style.width		= this.width;
+			newDiv.style.height		= this.height;
+			newDiv.style.opacity	= this.opacity
+			newDiv.style.zIndex		= this.zIndex;
 		
 		document.body.appendChild(newDiv);
-	
 	},
-	
+
+    /**
+     * Set params from server side
+     * @param settings
+     */
 	initSettings: function (settings) {		
-		this.layer_img = typeof(settings['maketImg'])!='undefined'?settings['maketImg']:false;
+		this.layer_img  = settings.maketImg ? settings.maketImg:'none';
+		this.top        = settings.top      ? settings.top:0;
+		this.left       = settings.left     ? settings.left:0;
+		this.zIndex     = settings.zIndex   ? settings.zIndex:1000;
+        this.width      = settings.width    ? settings.width:'100%';
+        this.height     = settings.height   ? settings.height:'1000px';
+        this.opacity    = settings.opacity  ? settings.opacity:'0.5';
+
 		//this.layer_wrap = '<div class="layer" id="upper-layer" style="background:url('+this.layer_img+') no-repeat 50% 0 transparent; display:none; position: absolute; top:0; left:0; width:100%; height: 800px; opacity:0.5;"></div>';
 	},
-	
+
+    /**
+     * Управление показом/сокрытием макета используя комбинацию клавишей Gtrl+<СтрелкаВверх>
+     * @param event
+     * @constructor
+     */
 	NavigateThrough: function (event) {
 		if (!document.getElementById) return;
 
@@ -51,8 +74,8 @@ var upperLayout = {
 			var maket = document.getElementById(upperLayout.layer_id);
 			switch (event.keyCode ? event.keyCode : event.which ? event.which : null){
 				case 0x26:			
-				maket.style.display = (maket.style.display != 'none')?'none':'block'; 
-					break;
+				    maket.style.display = (maket.style.display != 'none')?'none':'block';
+				break;
 			}
 		}			
 	}
